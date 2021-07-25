abb6193d7210d5e5e4f10b83e447bee9485cac8d
Greg Gonzalez <gregery.gonzalez@gmail.com>
2021-07-25 19:45:13 -0400

    create message

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -27,22 +27,31 @@ namespace ElevenNote.WebMVC.Controllers
             return View();
         }
 
+        // POST: Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
             {
-                return View(model);
-            }
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
 
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var service = new NoteService(userId);
+            ModelState.AddModelError("", "Note could not be created.");
 
-            service.CreateNote(model);
-
-            return RedirectToAction("Index");
+            return View(model);
         }
 
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
     }
 }
